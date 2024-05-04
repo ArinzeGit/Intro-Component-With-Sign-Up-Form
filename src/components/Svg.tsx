a979329303a9d8c91e375749d3cfb987e674d0e3
Arinze Owoh <arinzeowoh@gmail.com>
2024-05-04 23:42:14 +0100

    reimplement svg error icon

--- src/components/Svg.tsx
+++ src/components/Svg.tsx
@@ -1,18 +1,7 @@
 import "./Svg.css";
+import SvgSrc from "../assets/icon-error.svg";
 const Svg = () => {
-  return (
-    <div className="error-icon-container">
-      {
-        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-          <g fill="none" fillRule="evenodd">
-            <circle fill="#FF7979" cx="12" cy="12" r="12" />
-            <rect fill="#FFF" x="11" y="6" width="2" height="9" rx="1" />
-            <rect fill="#FFF" x="11" y="17" width="2" height="2" rx="1" />
-          </g>
-        </svg>
-      }
-    </div>
-  );
+  return <img className="error-icon" src={SvgSrc} alt="Error icon" />;
 };
 
 export default Svg;
